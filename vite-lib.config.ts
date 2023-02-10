d55b9029bc160809cee1b6be96de034d482d584a
Mikhail Lesin <mlesin@mrpost.ru>
2023-02-10 16:36:59 +0300

    add build stage to be able to build from github

--- vite-lib.config.ts
+++ vite-lib.config.ts
@@ -1,38 +1,40 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
-import dts from 'vite-plugin-dts';
-import pkg from './package.json';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
+import pkg from "./package.json";
 
-const path = require('path')
+const path = require("path");
 
 // On reprend le nom du module à partir de package.json :
 const name = pkg.name;
 
 // On reprend toutes les dépendances à externaliser de package.json :
-const external = Object.keys({...pkg.dependencies, ...pkg.peerDependencies} || {});
+const external = Object.keys(
+  { ...pkg.dependencies, ...pkg.peerDependencies } || {}
+);
 
 export default defineConfig({
-    base: '',
-    server: {
-        port: 8080
+  base: "",
+  server: {
+    port: 8080,
+  },
+  plugins: [
+    vue(),
+    dts({
+      skipDiagnostics: true,
+      include: ["lib/**/*", "lib/components/*"],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "lib/index.ts"),
+      name,
+      formats: ["es"],
+      fileName: (format) => name + `.${format}.js`,
     },
-    plugins: [
-        vue(),
-        dts({
-            logDiagnostics: true,
-            include: ['lib/**/*', 'lib/components/*'],
-        }),
-    ],
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, 'lib/index.ts'),
-            name,
-            formats: ['es'],
-            fileName: (format) => name + `.${format}.js`,
-        },
-        rollupOptions: {
-            external,
-            output: {}
-        }
-    }
-})
+    rollupOptions: {
+      external,
+      output: {},
+    },
+  },
+});
